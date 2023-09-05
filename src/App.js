4350535ecf5db1161391013f78f62acfbfbcee03
shubhambargal2002 <shubhambargal2002@gmail.com>
2023-09-05 23:26:15 +0530

    added react toastify notifications

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import "./App.css";
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [user,setUser]=useState({userName:"", address:""})
+  const [user, setUser] = useState({ userName: "", address: "" });
 
-    const handleSubmit = async (e)=>{
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const res = await axios.post("http://localhost:5000/user", user)
-            console.log(res);
-            console.log(user);
-            // if(res.status === 200){
-            //     toast.success("Registered Successfully...!")
-            // }
-        } catch (error) {
-            console.log(error);
-            if(error.response.status === 422){
-                // toast.error(error.response.data.error)
-                console.log(error.response.data.error);
-            }
-        }
-    }
-
-    const onChange=(e)=>{
-        setUser({...user,[e.target.name]:e.target.value})
+    try {
+      const res = await axios.post("http://localhost:5000/user", user);
+      if (res.status === 200) {
+        toast.success("User data added Successfully...!");
       }
+      setUser({ userName: "", address: "" });
+    } catch (error) {
+      console.log(error);
+      if (error.response.status === 422) {
+        toast.error(error.response.data.error);
+      }
+    }
+  };
+
+  const onChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
   return (
     <div className="user-outer">
       <div className="user">
@@ -50,6 +50,18 @@ function App() {
           </button>
         </form>
       </div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </div>
   );
 }
